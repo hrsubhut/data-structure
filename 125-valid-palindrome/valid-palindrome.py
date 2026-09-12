8412cc0c162b05a9f73b1af536a99a4db9c4da6a
Shobiiii <itstomars21@gmail.com>
2026-09-12 23:39:17 +0530

    Time: 12 ms (83.38%) | Memory: 14.2 MB (16.71%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,4 +1,5 @@
 class Solution(object):
+    #here what i do is normally make a two string and made check for it
     def isPalindrome(self, s):
         s = ''.join(char.lower()for char in s if char.isalnum())
         return s == s[::-1]
