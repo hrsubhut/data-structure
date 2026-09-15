af58058a6539fef509cdd2ed363ae4ecccc92a31
Shobiiii <itstomars21@gmail.com>
2026-09-16 00:21:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (69.22%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.py
@@ -0,0 +1,26 @@
+class MyStack(object):
+    
+    def __init__(self):
+        self.q =deque()
+        
+
+    def push(self, x):
+        self.q.append(x)
+        for i in range(len(self.q)-1):
+            self.q.append(self.q.popleft())
+        
+
+    def pop(self):
+        return self.q.popleft()
+        
+
+    def top(self):
+        return self.q[0]
+        
+
+    def empty(self):
+        if len(self.q)!=0:
+            return False
+        else:
+            return True
+        
